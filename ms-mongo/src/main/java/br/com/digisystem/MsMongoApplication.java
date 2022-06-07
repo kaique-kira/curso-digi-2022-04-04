956ad278af0e81682fce0f9f4a0e5828a9d9201c
Kaique Kira <83593559+kaique-kira@users.noreply.github.com>
2022-06-07 10:30:34 -0300

    chore: projeto ms mongo db

---
+++ ms-mongo/src/main/java/br/com/digisystem/MsMongoApplication.java
@@ -0,0 +1,13 @@
+package br.com.digisystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMongoApplication.class, args);
+	}
+
+}
---
+++ ms-mongo/src/test/java/br/com/digisystem/MsMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.digisystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
